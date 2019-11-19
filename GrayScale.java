f247e301d0506a7d2903b3c125561c635157d874
David H <43797198+fyreven0m@users.noreply.github.com>
2019-11-19 14:17:35 -0500

    Add files via upload

---
+++ GrayScale.java
@@ -0,0 +1,53 @@
+import java.util.Scanner;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+public class GrayScale {
+	public static void main(String args[]) throws IOException{
+		
+		Scanner kbReader = new Scanner(System.in);
+		BufferedImage img = null;
+		File f = null;
+		
+		try {
+			System.out.print("Enter image path: ");
+			String path = kbReader.next();
+			f = new File(path);
+			img = ImageIO.read(f);
+		}catch(IOException e) {
+			e.printStackTrace();
+		}
+		
+		int width = img.getWidth();
+		int height = img.getHeight();
+		
+		for(int x = 0; x < width; x++) {
+			for(int y = 0; y < height; y++) {
+				
+				int p = img.getRGB(x,y);
+				
+				int a = (p>>24) & 0xff;
+				int r = (p>>16) & 0xff;
+				int g = (p>>8) & 0xff;
+				int b = p & 0xff;
+				
+				int avg = (r+g+b)/3;
+				
+				p = (a<<24)|(avg<<16)|(avg<<8)|avg;
+				
+				img.setRGB(x, y, p);
+			}
+		}
+		
+		try{
+			f = new File("D:\\ImagesTester\\Out.jpg"); 
+			ImageIO.write(img, "jpg", f); 
+			System.out.println("Completed.");
+	    } catch(IOException e) { 
+	    	System.out.println(e); 
+	    } 
+	}
+}
